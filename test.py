261f91b13ab0d746aae33601aec3b2954237d7b2
Harish24-10-2005 <harishravikumar24@gmail.com>
2025-07-06 14:17:51 +0530

    Readhat Mcp is Added

--- test.py
+++ test.py
@@ -21,6 +21,18 @@ client = MultiServerMCPClient({
         },
         "disabled": False,
         "alwaysAllow":[]
+    },
+    "redhat": {
+        "command": "uv",
+        "args": [
+            "run",
+            "redhat_mcp_server.py"
+        ],
+        "cwd": "D:/Adya/MCPs_Server/redhat-api-mcp",
+        "transport": "stdio",
+        "env": {
+            "RH_API_OFFLINE_TOKEN": "eyJhbGciOiJIUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI0NzQzYTkzMC03YmJiLTRkZGQtOTgzMS00ODcxNGRlZDc0YjUifQ.eyJpYXQiOjE3NTE1MTU1OTQsImp0aSI6ImY5NjRmODExLWJlZmQtNDc0ZS1iNWUwLTYxNmYwMDI0NDRkYSIsImlzcyI6Imh0dHBzOi8vc3NvLnJlZGhhdC5jb20vYXV0aC9yZWFsbXMvcmVkaGF0LWV4dGVybmFsIiwiYXVkIjoiaHR0cHM6Ly9zc28ucmVkaGF0LmNvbS9hdXRoL3JlYWxtcy9yZWRoYXQtZXh0ZXJuYWwiLCJzdWIiOiJmOjUyOGQ3NmZmLWY3MDgtNDNlZC04Y2Q1LWZlMTZmNGZlMGNlNjpkZWF2YW5hdGhhbiIsInR5cCI6Ik9mZmxpbmUiLCJhenAiOiJyaHNtLWFwaSIsInNpZCI6IjUwNWU4YWUwLTZhNDEtNDVjMi05NmQ3LWU4NzUyOGIzMDE3OCIsInNjb3BlIjoiYmFzaWMgcm9sZXMgd2ViLW9yaWdpbnMgY2xpZW50X3R5cGUucHJlX2tjMjUgb2ZmbGluZV9hY2Nlc3MifQ.HnzQ70jYMO5n8378Sxdz6nEIpUrZjkROstvehzMbfmifKD9Nj9Dh__DE-Btnihasr0nHYKCItSxJm5Bo6zuCVQ"
+        }
     }
 })
 
@@ -28,7 +40,7 @@ async def main():
     tools = await client.get_tools()
     agent = create_react_agent(llm, tools)
     response = await agent.ainvoke({
-        "messages": [{"role": "user", "content": "what are the teams in my pagerduty"}]
+        "messages": [{"role": "user", "content": "what are tool u have access in redhat"}]
     })
     for i in response["messages"]:
         i.pretty_print()
